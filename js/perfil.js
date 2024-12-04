858779ed6a4ba7aa32436f65a94a924cfd637e21
Pedro Centeno Lopez <pedroiclop25@gmail.com>
2024-12-04 17:27:06 -0600

    Arreglo de html, css, js y phps duplicados

--- js/perfil.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const validacion = JSON.parse(localStorage.getItem("validado"));
-
-  const email = document.getElementById("email");
-  if (validacion) {
-    email.textContent = validacion.email;
-  }
-});
